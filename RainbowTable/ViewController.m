5a0deab2168ff69403ae0ca3da046dbdb2055d35
Syed Amaanullah <syedamaanullah@Syeds-MacBook-Pro.local>
2015-01-12 13:33:00 -0600

    Rainbow Table Hackwich - Week 2 Day 1

--- RainbowTable/ViewController.m
+++ RainbowTable/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+//This is a mutable array because we want our button to be able to add random colors to the array of colors
+@property NSMutableArray *colors;
+@property (weak, nonatomic) IBOutlet UITableView *colorTableView;
 
 @end
 
@@ -16,12 +19,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.colors = [NSMutableArray arrayWithObjects:[UIColor redColor], [UIColor orangeColor], [UIColor yellowColor], [UIColor greenColor], [UIColor blueColor], [UIColor colorWithRed:75/255.0 green:0.0/255.0 blue:130.0/255.0 alpha:1], [UIColor purpleColor], nil];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+//This pragma mark is simply to highlight the UITableViewDataSource in the organizational layout
+#pragma UITableViewDataSource
+
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.colors.count;
 }
+
+-(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ColorCellID"];
+    cell.textLabel.text = [NSString stringWithFormat:@"row %li", (long)indexPath.row];
+
+    cell.backgroundColor = [self.colors objectAtIndex:indexPath.row];
+
+    return cell;
+    }
+
+- (IBAction)onAddRandomColor:(id)sender {
+    //rand returns an integer,so we cast it as a float
+    //using rand and RAND_MAX ensure that the number will always be between 0 and 1.
+    CGFloat red = (float)rand() / RAND_MAX;
+    CGFloat green = (float)rand() / RAND_MAX;
+    CGFloat blue = (float)rand() / RAND_MAX;
+
+    UIColor *randomColor = [UIColor colorWithRed:red green:green blue:blue alpha:1];
+
+    [self.colors addObject:randomColor];
+    [self.colorTableView reloadData];
+    
+
+}
+
 
 @end
